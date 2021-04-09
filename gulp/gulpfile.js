c443d0dd7a72e417e37615a39b129cde6c5989ee
LimitDevelopers <helper@limit-project.com>
2021-04-09 21:02:27 +0700

    gulp(default)+gulp(Angular)

---
+++ gulp/gulpfile.js
@@ -0,0 +1,100 @@
+let project_folder="dist";
+let source_folder="src";
+
+let path={
+    build: {
+        html: project_folder+"/",
+        css: project_folder+"/css/",
+        js: project_folder+"/js/",
+        img: project_folder+"/img/",
+        fonts: project_folder+"/fonts/",
+    },
+
+    src: {
+        html: source_folder+"/*.html",
+        css: source_folder+"/css/style.css",
+        js: source_folder+"/js/scripts.js",
+        img: source_folder+"/img/*.{jpg,png,svg,ico,gif}",
+        fonts: source_folder+"/fonts/*.ttf",
+    }, 
+
+    watch: {
+        html: source_folder+"/**/*.html",
+        css: source_folder+"/css/**/*.css",
+        js: source_folder+"/js/**/*.js",
+        img: source_folder+"/img/*.{jpg,png,svg,ico,gif}",
+    },
+
+    clean: "./" + project_folder + "/"
+}
+
+let {src, dest} = require('gulp'),
+    gulp = require('gulp'),
+    browsersync = require("browser-sync").create(),
+    fileinclude = require("gulp-file-include"),
+    del = require("del"),
+    CSS = require("gulp-sass"),
+    clean_css = require("gulp-clean-css"),
+    rename = require("gulp-rename");
+
+function browserSync(params) {
+    browsersync.init({
+        server:{
+            baseDir: "./" + project_folder + "/"
+        },
+        port: 3000,
+        notify: false
+    })
+}
+
+function html() {
+    return src(path.src.html)
+        .pipe(fileinclude())
+        .pipe(dest(path.build.html))
+        .pipe(browsersync.stream())
+}
+
+function css() {
+    return src(path.src.css)
+        .pipe(
+            CSS({
+               outputStyle: "expanded" 
+            })
+        )
+        .pipe(dest(path.build.css))
+        .pipe(clean_css())
+        .pipe(
+            rename({
+                extname: ".min.css"
+            })
+        )
+        .pipe(dest(path.build.css))
+        .pipe(browsersync.stream())
+}
+
+function js() {
+    return src(path.src.js)
+        .pipe(fileinclude())
+        .pipe(dest(path.build.js))
+        .pipe(browsersync.stream())
+}
+
+function watchFiles(params) {
+    gulp.watch([path.watch.html], html);
+    gulp.watch([path.watch.css], css);
+    gulp.watch([path.watch.js], js);
+}
+
+function clean(params) {
+    return del(path.clean);
+}
+
+let build = gulp.series(clean, gulp.parallel(js, css, html));
+let watch = gulp.parallel(build, watchFiles, browserSync);
+
+exports.js = js;
+exports.css = css;
+exports.html = html;
+exports.build = build;
+exports.watch = watch;
+exports.default = watch;
